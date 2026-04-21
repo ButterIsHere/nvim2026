274ba4d9f4152b1dd713039e99cc0ef9b259bf49
ButterIsHere <akbarjaelani8500@gmail.com>
2026-04-22 05:38:46 +0700

    Set powershell as the default terminal if win32 == 1

--- init.lua
+++ init.lua
@@ -16,3 +16,16 @@ require("theme.theme")
 
 require("plugins.telescope")
 require("plugins.gitsigns")
+
+if vim.fn.has("win32") == 1 then
+    -- Use 'pwsh' if available, otherwise fall back to 'powershell'
+    local powershell_exe = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell'
+    
+    vim.opt.shell = powershell_exe
+    vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();"
+    vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellpipe = "2>&1 | Tee-Object %s; exit $LastExitCode"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+end
+
